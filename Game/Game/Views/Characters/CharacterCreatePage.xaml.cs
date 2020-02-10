4872475482a54f34e89eab66ed974e9b58fc2965
Spoorthi Bhat <spoorthicbhat@gmail.com>
2020-02-10 12:29:36 -0800

    Added contents of Constructor of CharacterCreatePage

--- Game/Game/Views/Characters/CharacterCreatePage.xaml.cs
+++ Game/Game/Views/Characters/CharacterCreatePage.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Game.Models;
+using Game.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -10,11 +13,25 @@ using Xamarin.Forms.Xaml;
 namespace Game.Views.Characters
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
+    [DesignTimeVisible(false)]
     public partial class CharacterCreatePage : ContentPage
     {
-        public CharacterCreatePage()
+        // the character to create
+        GenericViewModel<CharacterModel> ViewModel { get; set; }
+
+        public CharacterCreatePage(GenericViewModel<CharacterModel> data)
         {
             InitializeComponent();
+            data.Data = new CharacterModel();
+
+            BindingContext = this.ViewModel = data;
+
+            this.ViewModel.Title = "Create";
+
+            SpecialAbilityPicker.SelectedItem = data.Data.SpecialAbility.ToString();
+            AttackPicker.SelectedItem = data.Data.Attack.ToString();
+            DefensePicker.SelectedItem = data.Data.Defense.ToString();
+            HealthPicker.SelectedItem = data.Data.MaxHealth.ToString();
         }
 
         async void Save_Clicked(object sender, EventArgs e)
