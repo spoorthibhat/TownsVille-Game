4a10451a79f634b477d0d8f0bd7c329fcbfce20d
Sneha <annammohansn@seattleu.edu>
2020-02-09 16:08:08 -0800

    Adding files for Instruction page

---
+++ Game/Game/Views/Home/InstructionsPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game.Views
+{
+	/// <summary>
+	/// The Main Game Page
+	/// </summary>
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class InstructionsPage : ContentPage
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public InstructionsPage()
+		{
+			InitializeComponent ();
+		}
+
+		/// <summary>
+		/// Jump to the Dungeon
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+        async void OkButton_Clicked(object sender, EventArgs e)
+        {
+			
+		}
+	}
+}
