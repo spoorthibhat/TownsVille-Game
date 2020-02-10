5b95a11e8cb017624b8208466a6410352e1e6bd2
Spoorthi Bhat <spoorthicbhat@gmail.com>
2020-02-10 12:57:21 -0800

    Added IntStringConverter class

---
+++ Game/Game/Helpers/IntStringConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Game.Helpers
+{
+    class IntStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (int)value;
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+    }
+}
