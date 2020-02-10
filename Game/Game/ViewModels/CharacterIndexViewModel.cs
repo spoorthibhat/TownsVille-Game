40c5967049a05c8e2854398861db4516ee1ff5a5
NishigandhaMhatre <mhatrenishig@seattleu.edu>
2020-02-09 21:08:57 -0800

    Added CharacterIndexViewModel.cs

---
+++ Game/Game/ViewModels/CharacterIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Game.Models;
+using Game.Views;
+using System;
+using Xamarin.Forms;
+using System.Linq;
+using System.Collections.Generic;
+using Game.Services;
+
+
+namespace Game.ViewModels
+{
+    class CharacterIndexViewModel: BaseViewModel<CharacterModel>
+    {
+
+    }
+}
