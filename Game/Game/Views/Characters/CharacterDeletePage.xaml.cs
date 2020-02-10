fd8853ef2c75f2f0c7c9d4e6843797db9c79df0d
Sneha <annammohansn@seattleu.edu>
2020-02-10 13:24:27 -0800

    Added on click method for delete

--- Game/Game/Views/Characters/CharacterDeletePage.xaml.cs
+++ Game/Game/Views/Characters/CharacterDeletePage.xaml.cs
@@ -24,37 +24,26 @@ namespace Game.Views
 
             this.viewModel.Title = "Delete " + data.Title;
         }
-        /*
+        
         /// <summary>
-        /// Save calls to Update
+        /// Yes calls to Delete
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        async void Delete_Clicked(object sender, EventArgs e)
+        async void Yes_Clicked(object sender, EventArgs e)
         {
             MessagingCenter.Send(this, "Delete", viewModel.Data);
             await Navigation.PopModalAsync();
         }
-
+        
         /// <summary>
         /// Cancel and close this page
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        async void Cancel_Clicked(object sender, EventArgs e)
+        async void No_Clicked(object sender, EventArgs e)
         {
             await Navigation.PopModalAsync();
         }
-
-        /// <summary>
-        /// Trap the Back Button on the Phone
-        /// </summary>
-        /// <returns></returns>
-        protected override bool OnBackButtonPressed()
-        {
-            // Add your code here...
-            return true;
-        }
-        */
     }
 }
