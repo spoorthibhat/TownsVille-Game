cf63c139d69b5bc3975b4900d9562c749f09a9f9
NishigandhaMhatre <mhatrenishig@seattleu.edu>
2020-02-09 23:35:07 -0800

    Added Method to load default data in view model

--- Game/Game/ViewModels/CharacterIndexViewModel.cs
+++ Game/Game/ViewModels/CharacterIndexViewModel.cs
@@ -15,5 +15,37 @@ namespace Game.ViewModels
         {
             Title = "Character";
         }
+
+        /// <summary>
+        /// Returns the Character passed in
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public CharacterModel CheckIfCharacterExists(CharacterModel data)
+        {
+            // This will walk the items and find if there is one that is the same.
+            // If so, it returns the item...
+
+            var myList = Dataset.Where(a =>
+                                        a.Name == data.Name)
+                                        .FirstOrDefault();
+
+            if (myList == null)
+            {
+                // it's not a match, return false;
+                return null;
+            }
+
+            return myList;
+        }
+
+        /// <summary>
+        /// Load the Default Data
+        /// </summary>
+        /// <returns></returns>
+        public override List<CharacterModel> GetDefaultData()
+        {
+            return DefaultData.LoadData(new CharacterModel());
+        }
     }
 }
