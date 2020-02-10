aa73be28e00e2e0cc880209b1efe3685540f0e30
NishigandhaMhatre <mhatrenishig@seattleu.edu>
2020-02-09 23:58:03 -0800

    Added Constructor for CharacterIndexPage

--- Game/Game/Views/Characters/CharacterIndexPage.xaml.cs
+++ Game/Game/Views/Characters/CharacterIndexPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Game.Models;
+using Game.ViewModels;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,9 +14,19 @@ namespace Game.Views.Characters
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CharacterIndexPage : ContentPage
     {
+        // View Model used for data binding
+        readonly CharacterIndexViewModel ViewModel;
+
+
+        /// <summary>
+        /// Constructor for Character Page
+        /// 
+        /// Get the CharacterIndexView Model
+        /// </summary>
         public CharacterIndexPage()
         {
             InitializeComponent();
+            BindingContext = ViewModel = CharacterIndexViewModel.Instance;
         }
     }
 }
