3a4288790f4fa4978c9fa9bcbc0f696acde13a3f
Spoorthi Bhat <spoorthicbhat@gmail.com>
2020-02-10 11:00:09 -0800

    CharacterCreatePage partial commit

--- Game/Game/Views/Characters/CharacterCreatePage.xaml.cs
+++ Game/Game/Views/Characters/CharacterCreatePage.xaml.cs
@@ -16,5 +16,38 @@ namespace Game.Views.Characters
         {
             InitializeComponent();
         }
+
+        async void Save_Clicked(object sender, EventArgs e)
+        {
+            // TODO : Complete this
+            return;
+        }
+
+        async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            // TODO : Complete this
+            return;
+        }
+
+        void Level_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            // LevelValue.Text = String.Format("{0}", e.NewValue);
+        }
+
+        void Attack_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            // AttackValue.Text = String.Format("{0}", e.NewValue);
+        }
+
+        void Defense_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            // DefenseValue.Text = String.Format("{0}", e.NewValue);
+        }
+
+        void Health_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            // HealthValue.Text = String.Format("{0}", e.NewValue);
+        }
+
     }
 }
