5af5132ae24812917c82ac28b680f54b0eb662d7
Spoorthi Bhat <spoorthicbhat@gmail.com>
2020-02-11 16:51:19 -0800

    Added comments to IntStringConverter

--- Game/Game/Helpers/IntStringConverter.cs
+++ Game/Game/Helpers/IntStringConverter.cs
@@ -4,14 +4,35 @@ using Xamarin.Forms;
 
 namespace Game.Helpers
 {
+    /// <summary>
+    /// This converter is used by the Pickers, to change from the picker value to the integer value 
+    /// from string or vice versa. This allows the convert in the picker to be data bound back and forth the model
+    /// the picker requires this because the picker must be a string, but the enum is a value...
+    /// </summary>
     class IntStringConverter : IValueConverter
     {
+        /// <summary>
+        /// Parses the string to integer.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return (int)value;
         }
 
 
+        /// <summary>
+        /// Converts the integer value to string.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return value.ToString();
