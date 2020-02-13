b11070c49144ec0c778c75f3483c64e23c905d76
Spoorthi Bhat <spoorthicbhat@gmail.com>
2020-02-12 20:44:47 -0800

    Added MonsterCreatePage, partial commit

---
+++ Game/Game/Views/Monsters/MonsterCreatePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Game.Models;
+using Game.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game.Views.Monsters
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MonsterCreatePage : ContentPage
+    {
+        GenericViewModel<MonsterModel> ViewModel { get; set; }
+
+        public MonsterCreatePage(GenericViewModel<MonsterModel> data)
+        {
+            InitializeComponent();
+            data.Data = new MonsterModel();
+
+            BindingContext = this.ViewModel = data;
+
+            this.ViewModel.Title = "Create";
+        }
+    }
+}
