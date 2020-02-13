1600ce880078dd0bc002e9b6fe2cc3e07e4f7ece
NishigandhaMhatre <mhatrenishig@seattleu.edu>
2020-02-12 16:18:56 -0800

    Updated default data

--- Game/Game/Services/DefaultData.cs
+++ Game/Game/Services/DefaultData.cs
@@ -111,13 +111,7 @@ namespace Game.Services
         public static List<MonsterModel> LoadData(MonsterModel temp)
         {
             var datalist = new List<MonsterModel>()
-            { new MonsterModel{
-                Name = "Blossom",
-                Description = "Self-proclaimed leader of the Powerpuff Girls",
-                ImageURI = "Blossum.png",
-                Level = 1,
-
-            },
+            { 
             new MonsterModel{
                 Name = "Mojo jojo",
                 Description = "He is the master mind",
