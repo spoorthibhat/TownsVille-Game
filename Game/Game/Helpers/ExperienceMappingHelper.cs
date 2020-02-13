22cdb3d4aad346be55347c6921381d8de09a0402
Spoorthi Bhat <spoorthicbhat@gmail.com>
2020-02-13 11:20:33 -0800

    Added GetLevelPerExperience to ExperienceMappingHelper

--- Game/Game/Helpers/ExperienceMappingHelper.cs
+++ Game/Game/Helpers/ExperienceMappingHelper.cs
@@ -10,16 +10,22 @@ namespace Game.Helpers
     {
         static int[] Experiences = {0,300,900,2700,6500,14000,23000,34000,48000,64000,85000,100000,120000,140000,165000,195000,225000,265000,305000,355000};
 
-        public static Dictionary<int, int> GetExperienceLevelMapping()
-        {
-            var Experience_Level = new Dictionary<int, int>();
-            
-           for(int i = 1; i <=20; i++)
+
+        /// <summary>
+        /// Gets the level associated with the experience passed
+        /// </summary>
+        /// <param name="Experience"></param>
+        /// <returns></returns>
+        public static int GetLevelPerExperience(int Experience)
+        {  
+
+            int index = Array.BinarySearch(Experiences, Experience);
+            if(index < 0)
             {
-                Experience_Level.Add(Experiences[i], i);
+                index = ~index - 1;
             }
 
-            return Experience_Level;
+            return index + 1; // This would be the level since level starts with 1
         }
 
     }
