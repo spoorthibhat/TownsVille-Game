caf476b850aae8d96f63747ceda87f26c3dede81
Spoorthi Bhat <spoorthicbhat@gmail.com>
2020-02-13 18:57:36 -0800

    Added ItemModelHelper class

---
+++ Game/Game/Helpers/ItemModelHelper.cs
@@ -0,0 +1,24 @@
+﻿using Game.Models;
+using Game.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game.Helpers
+{
+    public static class ItemModelHelper
+    {
+        public static ItemModel GetItemModel(string id)
+        {
+            if(id != null)
+            {
+                return ItemIndexViewModel.Instance.Dataset.Where(a =>
+                                        a.Id == id)
+                                        .FirstOrDefault();
+            }
+
+            return null;
+        }
+    }
+}
