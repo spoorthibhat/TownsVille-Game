d262c5d25e654d6676fcaf2de1f413f8328f6013
Sneha <annammohansn@seattleu.edu>
2020-02-18 14:51:18 -0800

    Updated Special ability to hold the damage value

--- Game/Game/Models/Enum/SpecialAbilityEnum.cs
+++ Game/Game/Models/Enum/SpecialAbilityEnum.cs
@@ -2,16 +2,14 @@
 namespace Game.Models
 {
     /// <summary>
-    /// List of special abilities
+    /// List of special abilities, these are always used as attack items
     /// </summary>
     public enum SpecialAbilityEnum
     {
         Unknown = 0,
-        //Characters can have laser eyes as attack weapon
-        Laser_Eyes = 1,
-        //Charaters wearing armour can protect themselves from attack
-        Armour = 2,
-        //User can choose to have no special abilities
-        None = 3,
+        //Characters can have laser eyes as attack weapon, the value is the damage caused by character 
+        Laser_Eyes = 10,
+        //Charaters can have freeze as attack on monster, the value is the damage caused by character
+        Freeze = 15,
     }
 }
