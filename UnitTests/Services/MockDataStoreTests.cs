aae6c7191104c0226b7f0b90dba8d07fdbeaca42
Spoorthi Bhat <spoorthicbhat@gmail.com>
2020-02-23 12:20:11 -0800

    Added MockDataStore_SetNeedsRefresh_Valid_True_Should_Pass()

--- UnitTests/Services/MockDataStoreTests.cs
+++ UnitTests/Services/MockDataStoreTests.cs
@@ -38,6 +38,25 @@ namespace UnitTests.Services
             // Assert
             Assert.IsNotNull(result);
         }
+
+        [Test]
+        public async Task MockDataStore_SetNeedsRefresh_Valid_True_Should_Pass()
+        {
+            // Arrange
+            var originalState = await DataStore.GetNeedsInitializationAsync();
+
+            // Act
+            DataStore.NeedsInitialization = true;
+            var newState = await DataStore.GetNeedsInitializationAsync();
+
+            // Reset
+
+            // Turn it back to the original state
+            DataStore.NeedsInitialization = originalState;
+
+            // Assert
+            Assert.AreEqual(true, newState);
+        }
     }
 
     
