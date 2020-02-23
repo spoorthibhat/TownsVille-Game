c92a4e06855b786483425e03c2bbff7318d7dd7f
Spoorthi Bhat <spoorthicbhat@gmail.com>
2020-02-23 12:13:18 -0800

    Added MockDataStoreTest

---
+++ UnitTests/Services/MockDataStoreTests.cs
@@ -0,0 +1,44 @@
+﻿using Game.Models;
+using Game.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Services
+{
+    [TestFixture]
+    public class MockDataStoreTests
+    {
+        MockDataStore<ItemModel> DataStore;
+
+        [SetUp]
+        public void Setup()
+        {
+            DataStore = MockDataStore<ItemModel>.Instance;
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await DataStore.WipeDataListAsync();
+        }
+
+        [Test]
+        public void MockDataStore_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = DataStore;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+
+    
+}
