6486fc3b469782fc2d599e940560307a644c0ea1
Spoorthi Bhat <spoorthicbhat@gmail.com>
2020-02-23 12:39:47 -0800

    Added MockDataStore_Delete_InValid_Null_Should_Fail

--- UnitTests/Services/MockDataStoreTests.cs
+++ UnitTests/Services/MockDataStoreTests.cs
@@ -241,6 +241,33 @@ namespace UnitTests.Services
             Assert.AreEqual(false, result);
             Assert.AreEqual(2, DataStore.datalist.Count());
         }
+
+        [Test]
+        public async Task MockDataStore_Delete_InValid_Null_Should_Fail()
+        {
+            // Arrange
+            var item1 = new ItemModel
+            {
+                Name = "a"
+            };
+
+            var item2 = new ItemModel
+            {
+                Name = "b"
+            };
+
+            await DataStore.CreateAsync(item1);
+            await DataStore.CreateAsync(item2);
+
+            // Act
+            var result = await DataStore.DeleteAsync(null);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(2, DataStore.datalist.Count());
+        }
     }
 
     
