08529762ca2f2f41b38740bad141a25c22062beb
NishigandhaMhatre <mhatrenishig@seattleu.edu>
2020-02-25 14:19:48 -0800

    Added ImageModel_getURL_Default_Should_Pass() ImageModel_setURL_Default_Should_Pass()

--- UnitTests/Models/ImageModelTest.cs
+++ UnitTests/Models/ImageModelTest.cs
@@ -23,5 +23,34 @@ namespace UnitTests.Models
             // Assert 
             Assert.IsNotNull(result);
         }
+
+        [Test]
+        public void ImageModel_getURL_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new Image();
+            var data = result.Url;
+            // Reset
+
+            // Assert 
+            Assert.IsNull(data);
+        }
+
+        [Test]
+        public void ImageModel_setURL_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new Image();
+            result.Url = "new url";
+            var data = result.Url;
+            // Reset
+            result.Url = null;
+            // Assert 
+            Assert.AreEqual("new url",data);
+        }
     }
 }
