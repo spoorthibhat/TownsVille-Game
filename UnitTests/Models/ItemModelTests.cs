edc4fc74ff07a5c5f29ad73d2763db216433a8ea
NishigandhaMhatre <mhatrenishig@seattleu.edu>
2020-02-26 12:03:38 -0800

    Added tests  ItemModel_Update_Default_Should_Pass()

--- UnitTests/Models/ItemModelTests.cs
+++ UnitTests/Models/ItemModelTests.cs
@@ -64,7 +64,39 @@ namespace UnitTests.Models
             Assert.IsFalse(data);
         }
 
+        [Test]
+        public void ItemModel_Update_Default_Should_Pass()
+        {
+            // Arrange
+            var dataOriginal = new ItemModel();
+            dataOriginal.Value = 1;
 
+            var dataNew = new ItemModel();
+            dataNew.Value = 2;
+
+            // Act
+            var result = dataOriginal.Update(dataNew);
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(2, dataOriginal.Value);
+        }
+
+        [Test]
+        public void ItemModel_FormatOuput_Default_Should_Pass()
+        {
+            // Arrange
+            var data = new ItemModel();
+
+            // Act
+            var result = data.FormatOutput();
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual("This is an Item , Item Description for Unknown with Unknown+0 , Damage : 0 , Range : 0", result);
+        }
 
     }
 }
