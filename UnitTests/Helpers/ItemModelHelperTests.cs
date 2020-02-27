f783bb97c08c004af70f8d4ca175a49dee2e0933
Spoorthi Bhat <spoorthicbhat@gmail.com>
2020-02-27 14:57:57 -0800

    Added ItemModelHelper_GetItemModelFromGuid_Null_Should_ReturnNull to ItemModelHelperTests

---
+++ UnitTests/Helpers/ItemModelHelperTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Game.Helpers;
+
+namespace UnitTests.Helpers
+{
+    [TestFixture]
+    class ItemModelHelperTests
+    {
+        [Test]
+        public void ItemModelHelper_GetItemModelFromGuid_Null_Should_ReturnNull()
+        {
+            // act
+            var result = ItemModelHelper.GetItemModelFromGuid(null);
+
+            // assert
+            Assert.IsNull(result);
+        }
+    }
+}
