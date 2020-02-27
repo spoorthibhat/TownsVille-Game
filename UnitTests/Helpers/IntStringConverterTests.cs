3d6315e764faa3a1f0f72cdf301fa86355155c85
Spoorthi Bhat <spoorthicbhat@gmail.com>
2020-02-27 12:00:52 -0800

    Added IntString_Convert_Should_Skip to IntStringConverterTests

---
+++ UnitTests/Helpers/IntStringConverterTests.cs
@@ -0,0 +1,26 @@
+﻿using Game.Helpers;
+using Game.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Helpers
+{
+    [TestFixture]
+    class IntStringConverterTests
+    {
+        [Test]
+        public void IntString_Convert_Should_Skip()
+        {
+            var myConverter = new IntStringConverter();
+
+            var myObject = new ItemModel();
+
+            var Result = myConverter.Convert(myObject, null, null, null);
+            var Expected = 0;
+
+            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+        }
+    }
+}
