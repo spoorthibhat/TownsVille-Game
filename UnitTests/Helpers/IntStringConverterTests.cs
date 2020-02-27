bf0c4776cc99fb86bb1dda349f07d6b4e649fe83
Spoorthi Bhat <spoorthicbhat@gmail.com>
2020-02-27 12:14:14 -0800

    Added IntString_ConvertBack_Should_Pass to IntStringConverterTests

--- UnitTests/Helpers/IntStringConverterTests.cs
+++ UnitTests/Helpers/IntStringConverterTests.cs
@@ -43,8 +43,21 @@ namespace UnitTests.Helpers
 
             var myObject = new ItemModel();
 
-            var Result = myConverter.Convert(myObject, null, null, null);
+            var Result = myConverter.ConvertBack(myObject, null, null, null);
             var Expected = 0;
+
+            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+        }
+
+        [Test]
+        public void IntString_ConvertBack_Should_Pass()
+        {
+            var myConverter = new IntStringConverter();
+
+            var myObject = "20";
+
+            var Result = myConverter.ConvertBack(myObject, null, null, null);
+            var Expected = "20";
 
             Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
         }
