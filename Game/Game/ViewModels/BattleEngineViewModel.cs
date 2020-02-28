6f6bb40eb218a745cf39f25e2beb5d144ba72fad
Sneha <annammohansn@seattleu.edu>
2020-02-28 12:22:12 -0800

    Added initial version of Battle Engine View Model

---
+++ Game/Game/ViewModels/BattleEngineViewModel.cs
@@ -0,0 +1,61 @@
+﻿using Game.Models;
+using Game.Views;
+using System;
+using Xamarin.Forms;
+using System.Linq;
+using System.Collections.Generic;
+using Game.Services;
+
+namespace Game.ViewModels
+{
+    /// <summary>
+    /// Index View Model
+    /// Manages the list of data records
+    /// </summary>
+    public class BattleEngineViewModel
+    {
+        #region Singleton
+
+        // Make this a singleton so it only exist one time because holds all the data records in memory
+        private static volatile BattleEngineViewModel instance;
+        private static readonly object syncRoot = new Object();
+
+        public static BattleEngineViewModel Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new BattleEngineViewModel();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion Singleton
+
+
+        /// <summary>
+        /// The Battle Engine
+        /// </summary>
+        public Engine.BattleEngine Engine = new Engine.BattleEngine();
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public BattleEngineViewModel()
+        {
+        }
+
+        #endregion Constructor
+    }
+}
