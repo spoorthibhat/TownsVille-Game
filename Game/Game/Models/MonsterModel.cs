06fe7828c4de26e23383bcff4f2f36514f6fecc1
Spoorthi Bhat <spoorthicbhat@gmail.com>
2020-02-28 16:16:31 -0800

    Deleted the scale level

--- Game/Game/Models/MonsterModel.cs
+++ Game/Game/Models/MonsterModel.cs
@@ -65,23 +65,7 @@ namespace Game.Models
             return true;
         }
 
-        /// <summary>
-        /// Scales the level of the monster
-        /// </summary>
-        /// <param name="level"></param>
-        /// <returns></returns>
-        public bool ScaleLevel(int Level)
-        {
-            if (Level < 0)
-                return false;
-            this.Level = Level;
-            this.ExperiencePoints = LevelTableHelper.Instance.LevelDetailsList[Level].Experience;
-            this.Attack = LevelTableHelper.Instance.LevelDetailsList[Level].Attack;
-            this.Speed = LevelTableHelper.Instance.LevelDetailsList[Level].Speed;
-            this.Defense = LevelTableHelper.Instance.LevelDetailsList[Level].Defense;
-            this.MaxHealth = Level * 10 + Level;
-            return true;
-        }
+        
 
         /// <summary>
         /// Helper to combine the attributes into a single line, to make it easier to display the item as a string
