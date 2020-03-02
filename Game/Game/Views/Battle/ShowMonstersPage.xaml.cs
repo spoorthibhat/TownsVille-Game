b97923862ee03437a1b0fa3e93540e429a06b224
Spoorthi Bhat <spoorthicbhat@gmail.com>
2020-03-01 18:49:33 -0800

    Added listview to ShowMonsters page

--- Game/Game/Views/Battle/ShowMonstersPage.xaml.cs
+++ Game/Game/Views/Battle/ShowMonstersPage.xaml.cs
@@ -6,15 +6,32 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Game.ViewModels;
+using Game.Models;
+using Game.Engine;
 
 namespace Game.Views.Battle
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ShowMonstersPage : ContentPage
     {
+        // List of Monsters in the battle
+        public List<PlayerInfoModel> SelectedMonsterList;
+
+        // Constructor
         public ShowMonstersPage()
         {
             InitializeComponent();
+            var CharacterList = BattleEngineViewModel.Instance.SelectedCharacters;
+            var Battle = new BattleEngine();
+            foreach (CharacterModel Character in CharacterList)
+            {
+                Battle.PopulateCharacterList(Character);
+            }
+            Battle.StartBattle(false);
+
+            SelectedMonsterList = Battle.MonsterList;
+            //MonstersListView.ItemsSource = SelectedMonsterList;
         }
     }
 }
