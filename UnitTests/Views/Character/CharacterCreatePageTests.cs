8ded09ca36a940541dc3961da701534d75df355a
Spoorthi Bhat <spoorthicbhat@gmail.com>
2020-03-08 17:07:16 -0700

    CharacterCreatePageTests - partial commit

---
+++ UnitTests/Views/Character/CharacterCreatePageTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+using Game;
+using Game.Views;
+using Game.ViewModels;
+using Game.Models;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Mocks;
+
+namespace UnitTests.Views.Character
+{
+    [TestFixture]
+    class CharacterCreatePageTests : CharacterCreatePage
+    {
+        App app;
+        CharacterCreatePage page;
+
+        public CharacterCreatePageTests() : base(true) { }
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new CharacterCreatePage(new GenericViewModel<CharacterModel>(new CharacterModel()));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
+
+        [Test]
+        public void CharacterCreatePage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = page;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
