ab1d23f2e58294ecd0db9baed9418dbe82395e22
Spoorthi Bhat <spoorthicbhat@gmail.com>
2020-03-08 21:14:57 -0700

    Added Save_Clicked and Cancel_Clicked default tests

--- Game/Game/Views/Characters/CharacterCreatePage.xaml.cs
+++ Game/Game/Views/Characters/CharacterCreatePage.xaml.cs
@@ -76,7 +76,7 @@ namespace Game.Views
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        async void Save_Clicked(object sender, EventArgs e)
+        public async void Save_Clicked(object sender, EventArgs e)
         {
             // If the image in the data box is empty, use the default one..
             if (string.IsNullOrEmpty(ViewModel.Data.ImageURI))
@@ -98,7 +98,7 @@ namespace Game.Views
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        async void Cancel_Clicked(object sender, EventArgs e)
+        public async void Cancel_Clicked(object sender, EventArgs e)
         {
             await Navigation.PopModalAsync();
         }
--- UnitTests/Views/Character/CharacterCreatePageTests.cs
+++ UnitTests/Views/Character/CharacterCreatePageTests.cs
@@ -55,5 +55,34 @@ namespace UnitTests.Views.Character
             // Assert
             Assert.IsNotNull(result);
         }
+
+
+        [Test]
+        public void CharacterCreatePage_Cancel_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.Cancel_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void CharacterCreatePage_Save_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.Save_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
